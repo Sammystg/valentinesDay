b147bc9a450068779f855eaf61ba8a7960951ded
Samantha St-Germain <samantha.stgermain07@gmail.com>
2024-01-30 18:57:48 -0500

    altered moveButton function to take the margin into consideration and ensure the noButton does not ever move out of sight

--- scripts.js
+++ scripts.js
@@ -2,13 +2,27 @@ function nextPage() {
     window.location.href = "yes.html";
 }
 
-function moveButton() {
+// function moveButton() {
+//     var button = document.getElementById('noButton');
+//     var x = Math.random() * (window.innerWidth - button.offsetWidth);
+//     var y = Math.random() * (window.innerHeight - button.offsetHeight);
+//     button.style.left = `${x}px`;
+//     button.style.top = `${y}px`;
+//     console.log("moveButton function called");
+// }
+
+function moveButton(){
     var button = document.getElementById('noButton');
-    var x = Math.random() * (window.innerWidth - button.offsetWidth);
-    var y = Math.random() * (window.innerHeight - button.offsetHeight);
+    var margin = parseFloat(window.getComputedStyle(button).marginLeft);
+    var maxX = window.innerWidth - button.offsetWidth - margin;
+    var maxY = window.innerHeight - button.offsetHeight;
+
+    var x = Math.random() * maxX;
+    var y = Math.random() * maxY;
+
     button.style.left = `${x}px`;
-    button.style.top = `${y}px`;
-    console.log("moveButton function called");
+    button.style.top =`${y}px`;
+    console.log("moveButrton function called");
 }
 
 document.addEventListener('DOMContentLoaded', function() {
