3edab58c84318d77ed9f17b013e73e6b5bc789c2
Samantha St-Germain <samantha.stgermain07@gmail.com>
2024-01-31 18:13:44 -0500

    Created yes.html, this is where the user will be redirected to when they click on the yes button.

--- scripts.js
+++ scripts.js
@@ -2,15 +2,6 @@ function nextPage() {
     window.location.href = "yes.html";
 }
 
-// function moveButton() {
-//     var button = document.getElementById('noButton');
-//     var x = Math.random() * (window.innerWidth - button.offsetWidth);
-//     var y = Math.random() * (window.innerHeight - button.offsetHeight);
-//     button.style.left = `${x}px`;
-//     button.style.top = `${y}px`;
-//     console.log("moveButton function called");
-// }
-
 function moveButton(){
     var button = document.getElementById('noButton');
     var margin = parseFloat(window.getComputedStyle(button).marginLeft);
