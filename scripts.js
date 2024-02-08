54fb6c6037812491c86e466c8042fec72baa4480
Samantha St-Germain <samantha.stgermain07@gmail.com>
2024-02-07 19:32:43 -0500

    Ensure immeduate nmovement of no button on mobile screens.

--- scripts.js
+++ scripts.js
@@ -8,6 +8,9 @@ function moveButton(){
     var maxX = window.innerWidth - button.offsetWidth - margin;
     var maxY = window.innerHeight - button.offsetHeight;
 
+    maxX *= 0.8;
+    maxY *= 0.8;
+
     var x = Math.random() * maxX;
     var y = Math.random() * maxY;
 
@@ -17,11 +20,21 @@ function moveButton(){
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    moveButton();
+
     var button = document.getElementById('noButton');
 
     button.addEventListener('click', function(event) {
         event.preventDefault();
+        moveButton();
+    });
 
+    button.addEventListener('touchstart', function(event) {
+        event.preventDefault();
+        moveButton();
+    });
+
+    window.addEventListener('resize', function() {
         moveButton();
     });
 
