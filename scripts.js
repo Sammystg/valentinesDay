cc4c2a09517dfee4646ea0d078434f1fc36a77dd
Samantha St-Germain <samantha.stgermain07@gmail.com>
2024-02-08 19:08:50 -0500

    started JavaScript from scratch

--- scripts.js
+++ scripts.js
@@ -8,9 +8,6 @@ function moveButton(){
     var maxX = window.innerWidth - button.offsetWidth - margin;
     var maxY = window.innerHeight - button.offsetHeight;
 
-    maxX *= 0.8;
-    maxY *= 0.8;
-
     var x = Math.random() * maxX;
     var y = Math.random() * maxY;
 
@@ -20,22 +17,5 @@ function moveButton(){
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    moveButton();
-
-    var button = document.getElementById('noButton');
-
-    button.addEventListener('click', function(event) {
-        event.preventDefault();
-        moveButton();
-    });
-
-    button.addEventListener('touchstart', function(event) {
-        event.preventDefault();
-        moveButton();
-    });
-
-    window.addEventListener('resize', function() {
-        moveButton();
-    });
 
 });
