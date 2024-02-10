c7e9aa985d40b728cf0012ccc09b1bda122fe110
Samantha St-Germain <samantha.stgermain07@gmail.com>
2024-02-10 17:10:35 -0500

    Made adjustments to CSS to resize the gif on mobile and remove horizontal scrolling by setting the width of the gif container to a percentage of the viewport width and hiding overflow.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,3 @@
-// function nextPage() { 
-//     window.location.href = "yes.html";
-// }
-
-// function moveButton(){
-//     var button = document.getElementById('noButton');
-//     var margin = parseFloat(window.getComputedStyle(button).marginLeft);
-//     var maxX = window.innerWidth - button.offsetWidth - margin;
-//     var maxY = window.innerHeight - button.offsetHeight;
-
-//     var x = Math.random() * maxX;
-//     var y = Math.random() * maxY;
-
-//     button.style.left = `${x}px`;
-//     button.style.top =`${y}px`;
-//     console.log("moveButrton function called");
-// }
-
-// document.addEventListener('DOMContentLoaded', function() {
-
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     var noButton = document.getElementById('noButton');
 
